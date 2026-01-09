82b9e275cbcc7f54a989986d61ff19cce4937cf7
Deep Shah <deepshah003@gmail.com>
2026-01-09 14:03:12 +0530

    feat: [GREEN] support summation of two numbers

--- lib/string_calculator_kata.dart
+++ lib/string_calculator_kata.dart
@@ -1,6 +1,12 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+
+    if (numbers.contains(',')) {
+      var parts = numbers.split(',');
+      return int.parse(parts[0]) + int.parse(parts[1]);
+    }
+
     return int.parse(numbers);
   }
 }
