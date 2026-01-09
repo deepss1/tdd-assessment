04ee018d99304d3e05d6d67f421eff755022345d
Deep Shah <deepshah003@gmail.com>
2026-01-09 14:01:52 +0530

    test: [RED] add test case for single number

--- test/string_calculator_kata_test.dart
+++ test/string_calculator_kata_test.dart
@@ -9,5 +9,9 @@ void main() {
     test('should return 0 for an empty string', () {
       expect(calculator.add(""), 0);
     });
+
+    test('should return the number itself for a single number', () {
+      expect(calculator.add("1"), 1);
+    });
   });
 }
