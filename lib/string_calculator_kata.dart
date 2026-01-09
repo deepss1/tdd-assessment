3a2da7336d1f8791d1604b50428a4e188a6bbb4b
Deep Shah <deepshah003@gmail.com>
2026-01-09 14:08:08 +0530

    feat: [GREEN] support newline as a valid delimiter

--- lib/string_calculator_kata.dart
+++ lib/string_calculator_kata.dart
@@ -2,6 +2,8 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
+    var clean = numbers.replaceAll('\n', ',');
+
+    return clean.split(',').map(int.parse).reduce((a, b) => a + b);
   }
 }
