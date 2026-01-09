a15b2b108599fe2c3f10427d32cd996b0c51ff1f
Deep Shah <deepshah003@gmail.com>
2026-01-09 14:20:41 +0530

    feat: [GREEN] validate and throw on negative inputs

--- lib/string_calculator_kata.dart
+++ lib/string_calculator_kata.dart
@@ -8,14 +8,25 @@ class StringCalculator {
     // Check for custom delimiters
     if (numbers.startsWith("//")) {
       final parts = numbers.split('\n');
-      
+
       delimiter = parts[0].substring(2);
-      
+
       content = parts.sublist(1).join('\n');
     }
 
     content = content.replaceAll('\n', delimiter);
 
-    return content.split(delimiter).map(int.parse).reduce((a, b) => a + b);
+    List<int> numberList = content.split(delimiter).map(int.parse).toList();
+
+    _checkNegatives(numberList);
+
+    return numberList.reduce((a, b) => a + b);
+  }
+
+  void _checkNegatives(List<int> numbers) {
+    final negatives = numbers.where((n) => n < 0).toList();
+    if (negatives.isNotEmpty) {
+      throw Exception("negative numbers not allowed ${negatives.join(', ')}");
+    }
   }
 }
